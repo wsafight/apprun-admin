9a6f1198ca781148ba7bdec89dcdef9b90528031
ShianWang <984292420@qq.com>
2021-03-27 13:30:27 +0800

    feat: minify AppRun

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,10 @@
-
 import app from 'apprun';
 
 app.on('//', (route) => {
   const menus = document.querySelectorAll('.navbar-nav li');
-  for (let i = 0; i < menus.length; ++i) {menus[i].classList.remove('active');}
+  for (let i = 0; i < menus.length; ++i) {
+    menus[i].classList.remove('active');
+  }
   const item = document.querySelector(`[href='${route}']`);
   item && item.parentElement.classList.add('active');
 });
@@ -11,8 +12,9 @@ app.on('//', (route) => {
 export default () => <div class="container">
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">Project Name</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-      aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import Layout from './Layout';
 app.render(document.body, <Layout />);
 
 const element = 'my-app';
+
 new Home().start(element);
 new About().mount(element);
 new Contact().mount(element);
